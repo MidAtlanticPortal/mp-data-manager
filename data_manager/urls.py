9de403b8eac010fe29ee5f185aa6c34ef9ac97dc
Eric Drechsel <eric@pointnineseven.com>
2014-11-26 15:50:37 -0700

    change URLs so they only match exactly (was prefix match)

--- data_manager/urls.py
+++ data_manager/urls.py
@@ -3,6 +3,6 @@ from views import *
 
 urlpatterns = patterns('',
     (r'^layer/([A-Za-z0-9_-]+)$', update_layer),
-    (r'^layer', create_layer),
-    (r'^get_json', get_json)
+    (r'^layer$', create_layer),
+    (r'^get_json$', get_json)
 )
