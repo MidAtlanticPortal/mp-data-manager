d988262d807e360285522c0e51ed3b901fa0b4df
Ryan Hodges <ryan.d.hodges@gmail.com>
2016-12-20 14:02:36 -0800

    starting from scratch

--- data_manager/migrations/0001_initial.py
+++ data_manager/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import data_manager.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('sites', '0001_initial'),
     ]
 
     operations = [
@@ -46,12 +48,18 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=100)),
+                ('order', models.PositiveSmallIntegerField(default=10, help_text=b'input an integer to determine the priority/order of the layer being displayed (1 being the highest)', null=True, blank=True)),
                 ('slug_name', models.CharField(max_length=100, null=True, blank=True)),
                 ('layer_type', models.CharField(help_text=b'use placeholder to temporarily remove layer from TOC', max_length=50, choices=[(b'XYZ', b'XYZ'), (b'WMS', b'WMS'), (b'ArcRest', b'ArcRest'), (b'radio', b'radio'), (b'checkbox', b'checkbox'), (b'Vector', b'Vector'), (b'placeholder', b'placeholder')])),
                 ('url', models.CharField(max_length=255, null=True, blank=True)),
                 ('shareable_url', models.BooleanField(default=True, help_text=b'Indicates whether the data layer (e.g. map tiles) can be shared with others (through the Map Tiles Link)')),
                 ('arcgis_layers', models.CharField(help_text=b'comma separated list of arcgis layer IDs', max_length=255, null=True, blank=True)),
+                ('disable_arcgis_attributes', models.BooleanField(default=False, help_text=b'Click to disable clickable ArcRest layers')),
+                ('wms_slug', models.CharField(max_length=255, null=True, blank=True)),
+                ('wms_version', models.CharField(help_text=b'WMS Versioning - usually either 1.1.1 or 1.3.0', max_length=10, null=True, blank=True)),
                 ('is_sublayer', models.BooleanField(default=False)),
+                ('search_query', models.BooleanField(default=False, help_text=b'Select when layers are queryable - e.g. MDAT and CAS')),
+                ('has_companion', models.BooleanField(default=False, help_text=b'Check if this layer has a companion layer')),
                 ('is_disabled', models.BooleanField(default=False, help_text=b'when disabled, the layer will still appear in the TOC, only disabled')),
                 ('disabled_message', models.CharField(max_length=255, null=True, blank=True)),
                 ('legend', models.CharField(help_text=b'URL or path to the legend image file', max_length=255, null=True, blank=True)),
@@ -80,12 +88,14 @@ class Migration(migrations.Migration):
                 ('vector_color', models.CharField(max_length=7, null=True, blank=True)),
                 ('vector_fill', models.FloatField(null=True, blank=True)),
                 ('vector_graphic', models.CharField(max_length=255, null=True, blank=True)),
+                ('point_radius', models.IntegerField(help_text=b'Used only for for Point layers (default is 2)', null=True, blank=True)),
                 ('opacity', models.FloatField(default=0.5, null=True, blank=True)),
                 ('attribute_fields', models.ManyToManyField(to='data_manager.AttributeInfo', null=True, blank=True)),
+                ('connect_companion_layers_to', models.ManyToManyField(help_text=b'Select which main layer(s) you would like to use in conjuction with this companion layer.', related_name='connect_companion_layers_to_rel_+', null=True, to='data_manager.Layer', blank=True)),
             ],
             options={
             },
-            bases=(models.Model,),
+            bases=(models.Model, data_manager.models.SiteFlags),
         ),
         migrations.CreateModel(
             name='LookupInfo',
@@ -107,6 +117,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('display_name', models.CharField(max_length=100)),
                 ('name', models.CharField(max_length=100)),
+                ('order', models.PositiveSmallIntegerField(default=10, help_text=b'input an integer to determine the priority/order of the layer being displayed (1 being the highest)', null=True, blank=True)),
                 ('visible', models.BooleanField(default=True)),
                 ('header_image', models.CharField(max_length=255, null=True, blank=True)),
                 ('header_attrib', models.CharField(max_length=255, null=True, blank=True)),
@@ -118,15 +129,22 @@ class Migration(migrations.Migration):
                 ('feature_image', models.CharField(max_length=255, null=True, blank=True)),
                 ('feature_excerpt', models.TextField(null=True, blank=True)),
                 ('feature_link', models.CharField(max_length=255, null=True, blank=True)),
+                ('site', models.ManyToManyField(to='sites.Site')),
             ],
             options={
             },
-            bases=(models.Model,),
+            bases=(models.Model, data_manager.models.SiteFlags),
         ),
         migrations.AddField(
             model_name='layer',
             name='lookup_table',
             field=models.ManyToManyField(to='data_manager.LookupInfo', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='layer',
+            name='site',
+            field=models.ManyToManyField(to='sites.Site'),
             preserve_default=True,
         ),
         migrations.AddField(
