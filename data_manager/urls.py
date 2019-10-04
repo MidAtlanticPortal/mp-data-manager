a5cec392718a9546b275a3d98c4f01e222a6d88a
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-10-03 18:16:09 -0700

    supporting get_layer_details and properly querying sublayers

--- data_manager/urls.py
+++ data_manager/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
     (r'^get_themes$', views.get_themes),
     (r'^get_layer_search_data$', views.get_layer_search_data),
     (r'^get_layers_for_theme/(?P<themeID>\d+)$', views.get_layers_for_theme),
+    (r'^get_layer_details/(?P<layerID>\d+)$', views.get_layer_details),
     (r'^wms_capabilities', views.wms_request_capabilities),
 )
