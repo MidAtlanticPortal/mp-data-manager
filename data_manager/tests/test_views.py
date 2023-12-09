e957343837efccc018b25763af4a17e980ef6f65
Anna Cho <acho@ecotrust.org>
2023-12-08 16:32:35 -0800

    Added first test for get_json

--- data_manager/tests/test_views.py
+++ data_manager/tests/test_views.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.test.client import RequestFactory
+from data_manager.models import Layer
+from data_manager.views import get_json
+from collections.abc import Collection, Mapping
 import json
 
-from data_manager.models import Layer, Theme
+from data_manager.models import Layer
 
 class get_layer_details_test(TestCase):
     @classmethod
@@ -13,14 +16,37 @@ class get_layer_details_test(TestCase):
     def test_view_api_returns_zoom_limits(self):
         response = self.client.get('/data_manager/get_layer_details/1')
         self.assertEqual(response.status_code, 200)
-        # import ipdb; ipdb.set_trace()
         result = json.loads(response.getvalue())
         self.assertTrue('name' in result.keys())
         self.assertEqual(result['name'], 'arcrest_layer')
+        #checking that the attribute EXISTS in result.keys, if it does exist, it returns true
         self.assertTrue('minZoom' in result.keys())
         self.assertTrue('maxZoom' in result.keys())
         self.assertEqual(result['minZoom'], 6)
         self.assertEqual(result['maxZoom'], 14)
         print('Hello World')
 
-class get_json_test(TestCase):
+class data_manager_get_json_test(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+
+    def test_view_api_returns_zoom_limits(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/data_manager/get_json')
+        request.META['HTTP_HOST'] = "localhost:8000"
+        response = get_json(request)
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.content)
+
+        # Make sure response contains "success", "state", "layers" and "themes" 
+        self.assertIn("success", result)
+        self.assertIn("state", result)
+        self.assertIn("layers", result)
+        self.assertIn("themes", result)
+
+        self.assertTrue(result['success'])
+        self.assertIsInstance(result['layers'], Collection)
+        self.assertIsInstance(result['themes'], Collection)
+        self.assertIsInstance(result['state'], Mapping)
+      
